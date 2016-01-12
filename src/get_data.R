4ac81ff226843a06f5a8f8c711d7852df74438a7
Kris Sankaran <sankaran.kris@gmail.com>
2016-01-11 18:08:11 -0800

    save data to a public google drive folder

--- src/get_data.R
+++ src/get_data.R
@@ -7,6 +7,7 @@
 library("biom")
 library("phyloseq")
 path <- "~/Documents/data/microbiomeData" # path to repository
+data_path <- "~/Google Drive/microbiomeData/data" # path to data
 setwd(path)
 all_data <- list()
 
@@ -19,7 +20,7 @@ all_data$esophagus <- get(data(esophagus))
 ## ----  Waste not, want not paper ----
 
 # not sure how to download directly from qiita, but links are included. Include
-# the downloaded files in the repo's "data" directory
+# the downloaded files in the "data_path" directory
 # Global Patterns (already included)
 # cigarette smokers (http://qiita.microbio.me/study/description/524)
 # long term diet (http://qiita.microbio.me/study/description/1010)
@@ -27,8 +28,8 @@ all_data$esophagus <- get(data(esophagus))
 # restroom (http://qiita.microbio.me/study/description/1335)
 # note: none of these have sample tables, and the others mentioned in the paper
 # are not available from Qiita
-waste_not_names <- paste0("data/", c("306_", "145_", "246_", "220_"), "otu_table.biom")
-waste_not_physeq <- sapply(file.path(path, waste_not_names), import_biom)
+waste_not_names <- paste0(data_path, "/", c("306_", "145_", "246_", "220_"), "otu_table.biom")
+waste_not_physeq <- sapply(waste_not_names, import_biom)
 names(waste_not_physeq) <- c("smokers", "diet", "bokulich", "restroom")
 all_data <- c(all_data, waste_not_physeq)
 
@@ -40,4 +41,4 @@ all_data$pregnancy <- get(load(tmp))
 
 ## ---- save-and-upload ----
 dir.create("data")
-save(all_data, file = "data/microbiomeData.RData")
+save(all_data, file = file.path(data_path, "microbiomeData.RData"))
