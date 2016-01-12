b054bc6b808704dd745589070966452d338679f7
Kris Sankaran <sankaran.kris@gmail.com>
2016-01-11 17:55:28 -0800

    save data into repo

--- src/get_data.R
+++ src/get_data.R
@@ -18,7 +18,7 @@ all_data$esophagus <- get(data(esophagus))
 ## ----  Waste not, want not paper ----
 
 # not sure how to download directly from qiita, but links are included. Include
-# the downloaded files in the repo's directory
+# the downloaded files in the repo's "data" directory
 # Global Patterns (already included)
 # cigarette smokers (http://qiita.microbio.me/study/description/524)
 # long term diet (http://qiita.microbio.me/study/description/1010)
@@ -26,7 +26,7 @@ all_data$esophagus <- get(data(esophagus))
 # restroom (http://qiita.microbio.me/study/description/1335)
 # note: none of these have sample tables, and the others mentioned in the paper
 # are not available from Qiita
-waste_not_names <- paste(c("306", "145", "246", "220"), "otu_table.biom", sep = "_")
+waste_not_names <- paste("data", c("306", "145", "246", "220"), "otu_table.biom", sep = "_")
 waste_not_physeq <- sapply(file.path(path, waste_not_names), import_biom)
 names(waste_not_physeq) <- c("smokers", "diet", "bokulich", "restroom")
 all_data <- c(all_data, waste_not_physeq)
@@ -36,3 +36,7 @@ pregnancy_path <- "http://statweb.stanford.edu/~susan/papers/Pregnancy/Pregnancy
 tmp <- tempfile()
 download.file(pregnancy_path, tmp)
 all_data$pregnancy <- get(load(tmp))
+
+## ---- save-and-upload ----
+dir.create("data")
+save(all_data, file = "data/microbiomeData.RData")
