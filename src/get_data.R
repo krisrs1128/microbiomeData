a31d3630c57e36be22822d06f4f127799167d3c0
Kris Sankaran <sankaran.kris@gmail.com>
2016-01-12 13:11:47 -0800

    revise getting / uploading to use rdrop2

--- src/get_data.R
+++ src/get_data.R
@@ -6,9 +6,8 @@
 ## ---- setup ----
 library("biom")
 library("phyloseq")
-path <- "~/Documents/data/microbiomeData" # path to repository
-data_path <- "~/Google Drive/microbiomeData/data" # path to data
-setwd(path)
+library("RCurl")
+library("rdrop2")
 all_data <- list()
 
 ## ---- from-phyloseq ---
@@ -19,8 +18,8 @@ all_data$esophagus <- get(data(esophagus))
 
 ## ----  Waste not, want not paper ----
 
-# not sure how to download directly from qiita, but links are included. Include
-# the downloaded files in the "data_path" directory
+# not sure how to download directly from qiita, but I put some of the biome
+# files on a public dropbox, which should be accessible to anyone
 # Global Patterns (already included)
 # cigarette smokers (http://qiita.microbio.me/study/description/524)
 # long term diet (http://qiita.microbio.me/study/description/1010)
@@ -28,9 +27,20 @@ all_data$esophagus <- get(data(esophagus))
 # restroom (http://qiita.microbio.me/study/description/1335)
 # note: none of these have sample tables, and the others mentioned in the paper
 # are not available from Qiita
-waste_not_names <- paste0(data_path, "/", c("306_", "145_", "246_", "220_"), "otu_table.biom")
-waste_not_physeq <- sapply(waste_not_names, import_biom)
-names(waste_not_physeq) <- c("smokers", "diet", "bokulich", "restroom")
+
+# download the links to a temporary directory
+biom_paths <- c("https://dl.dropboxusercontent.com/s/26fuy3cxy428ntp/145_otu_table.biom",
+                "https://dl.dropboxusercontent.com/s/xfglsn14x09wlf4/220_otu_table.biom",
+                "https://dl.dropboxusercontent.com/s/58pgbiivr2vu48s/246_otu_table.biom",
+                "https://dl.dropboxusercontent.com/s/viuz6usj09qn8vm/306_otu_table.biom")
+sapply(biom_paths, function(x) {
+  download.file(x , dest = file.path(tempdir(), basename(x)),
+                method = "libcurl")
+})
+
+# import these bioms into phyloseq objects
+waste_not_physeq <- sapply(list.files(tempdir(), "*.biom", full = T), import_biom)
+names(waste_not_physeq) <- c("diet", "restroom", "bokulich", "smokers")
 all_data <- c(all_data, waste_not_physeq)
 
 ## ---- temporal-spatial-pregnancy ----
@@ -41,4 +51,6 @@ all_data$pregnancy <- get(load(tmp))
 
 ## ---- save-and-upload ----
 dir.create("data")
-save(all_data, file = file.path(data_path, "microbiomeData.RData"))
+save(all_data, file = file.path(tempdir(), "microbiomeData.RData"))
+drop_upload(file.path(tempdir(), "microbiomeData.RData"), "microbiomeData/data")
+drop_share("microbiomeData/data/microbiomeData.RData")
